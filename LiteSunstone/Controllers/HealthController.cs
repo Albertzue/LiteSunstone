0bdf85e07d879a2a2abfb5affe644b06ebb45700
Yunjie-Zhu <Yunjie.Zhu@carrier.com>
2023-08-23 08:14:17 +0800

    add health endpoint

---
+++ LiteSunstone/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiteSunstone.Api.Controllers
+{
+    [ApiController]
+    public class HealthController : Controller
+    {
+        [HttpGet("/health")]
+        public IActionResult Health()
+        {
+            return Ok();
+        }
+    }
+}
